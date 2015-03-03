12fc0b1428a9ce7a6494ceed50020b3dce5de161
calden <mickaeldebonnaire@gmail.com>
2015-03-04 00:53:58 +0100

    modification mineur commit

--- client/app/game/game.directive.js
+++ client/app/game/game.directive.js
@@ -1,15 +1,25 @@
 'use strict';
 
 angular.module('ticTacToeApp')
-      .directive('gameBoard', function factory() {
-        function controllerGameBoard($scope) {
-
+      .directive('gameBoard', [ function(){
+        function controllerGameBoard ($scope) {
+          // body...
         };
-        var directiveDefinitionObject = {
-          restrict: 'E',
-          replace: true,
+        // Runs during compile
+        return {
+          // name: '',
+          // priority: 1,
+          // terminal: true,
+          // scope: {}, // {} = isolate, true = child, false/undefined = no change
+          // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+          restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
           controller: controllerGameBoard,
           templateUrl: 'app/game/game.template.html',
+          replace: true,
+          // transclude: true,
+          // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+          link: function($scope, iElm, iAttrs, controller) {
+            
+          }
         };
-        return directiveDefinitionObject;
-      });
+      }]);
