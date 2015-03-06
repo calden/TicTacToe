feaaa74bac9f75c3c462c62aae347d2cbb303d63
Christophe Jollivet <christophe.jollivet@gmail.com>
2015-03-06 17:36:49 +0100

    added socket management of games

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,11 +1,16 @@
 'use strict';
 
 angular.module('ticTacToeApp')
-  .controller('MainCtrl',['$scope', '$state','games',function ($scope,$state,games) {
+  .controller('MainCtrl',['$scope', '$state','games', 'socket',function ($scope,$state,games, socket) {
+    socket.manageGames(games);
     $scope.select = function (game) {
       $scope.currentGame = game;
       $state.go("main.gameboard");
     }
     $scope.games = games ;
+
+    $scope.$on('$destroy', function(){
+      socket.removeListeners();
+    })
 
   }]);
--- server/api/game/game.socket.js
+++ server/api/game/game.socket.js
@@ -8,17 +8,12 @@ var Game = require('./game.model');
 
 exports.register = function(socket) {
   Game.schema.post('save', function (doc) {
-    onSave(socket, doc);
+    socket.emit('game:save', doc);
   });
   Game.schema.post('remove', function (doc) {
-    onRemove(socket, doc);
+    socket.emit('game:remove', doc);
   });
-}
-
-function onSave(socket, doc, cb) {
-  socket.emit('game:save', doc);
-}
-
-function onRemove(socket, doc, cb) {
-  socket.emit('game:remove', doc);
+  Game.schema.post('create', function(doc){
+    socket.emit('game:create', doc)
+  })
 }
