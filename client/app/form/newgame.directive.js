e29aed651781ef9df65d9b668c7a8c1b65f4e260
calden <mickaeldebonnaire@gmail.com>
2015-03-06 17:48:11 +0100

    Formulaire de creation de jeu

---
+++ client/app/form/newgame.directive.js
@@ -0,0 +1,34 @@
+'use strict';
+
+angular.module('ticTacToeApp')
+      .controller('controllerNewGame',[
+        '$scope',
+        'Auth',
+        'Game',
+        function ($scope,Auth,Game) {
+          $scope.userConnected = angular.isDefined(Auth.getCurrentUser().name);
+          $scope.gameCreated = false;
+          $scope.firstPlayer = true;
+          $scope.newGame = {
+            player1: "Test",
+            player2: ""
+          };
+          $scope.validateNewGame = function () {
+            $scope.newGame.turnPlayer = $scope.firstPlayer ? 1 : 2;
+            Game.save($scope.newGame);
+            $scope.gameCreated = true;
+          };
+
+        }])
+      .directive('newGame', [ function(){
+
+        return {
+          restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+          controller: 'controllerNewGame',
+          templateUrl: 'app/form/newgame.template.html',
+          replace: true,
+          link: function($scope, iElm, iAttrs, controller) {
+
+          }
+        };
+      }]);
