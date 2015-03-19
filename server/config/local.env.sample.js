412a34ca4430ea10243b5e6c061db9ebf0d51218
calden <mickaeldebonnaire@gmail.com>
2015-03-19 18:09:52 +0100

    modifyOauth

--- server/config/local.env.sample.js
+++ server/config/local.env.sample.js
@@ -9,8 +9,8 @@ module.exports = {
   DOMAIN:           'http://localhost:9000',
   SESSION_SECRET:   'tictactoe-secret',
 
-  GOOGLE_ID:        'app-id',
-  GOOGLE_SECRET:    'secret',
+  GOOGLE_ID:        '334871074181-r65fpjao9g9qsq1j43pnodvb05pq5r6n.apps.googleusercontent.com',
+  GOOGLE_SECRET:    'mJple3-pnfqYQ9ozgSdPMF5Z',
 
   // Control debug level for modules using visionmedia/debug
   DEBUG: ''
